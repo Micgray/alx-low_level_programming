3db4e1b7a1a6346634769343ed827bb663a09c89
Micgray <micgray7@gmail.com>
2022-07-10 06:47:59 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints putchar
- * Return: always 0 
+ * Return: always 0
  */
 int main(void)
 {
@@ -16,4 +16,4 @@ _putchar('a');
 _putchar('r');
 _putchar('\n');
 return (0);
-}
+} 
