5c0b52fc3478cc1448ab9aedc9a45d7f279d8d81
Micgray <micgray7@gmail.com>
2022-07-14 01:25:56 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the english alphabet
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+char i;
+char x;
+for (x = 0; x < 10; x++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
