ec96c500abde1142981246862ee1be32add2c94b
Micgray <micgray7@gmail.com>
2022-07-15 15:46:14 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,28 +1,18 @@
 #include "main.h"
+#include <string.h>
 
 /**
- * rev_string-> reversing a string
- * @s: parameter s
+ * puts2 -> puts2 function
+ * @str: str parameter
  */
 
-void rev_string(char *s)
+void puts2(char *str)
 {
-char tmp;
-int i, length1, length2;
+int len, i;
 
-length1 = 0;
-length2 = 0;
+len = strlen(str);
+for (i = 0; i < len; i += 2)
+_putchar(str[i]);
+_putchar('\n');
 
-while (s[length1] != '\0')
-length1++;
-
-length2 = length1 - 1;
-for (i = 0; i < length1 / 2; i++)
-{
-tmp = s[i];
-s[i] = s[length2];
-s[length2] = tmp;
-length2 -= 1;
-
-}
 }
