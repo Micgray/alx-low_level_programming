dd37ab2f37e3f89f95575756a700faf9fa1519bf
Micgray <micgray7@gmail.com>
2022-07-23 09:29:05 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset -> function that copies memory area
+ * @s: source string
+ * @b: the contant byte for filling
+ * @n: lenght of buffer 
+ * Return: new string 
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int 1 = 0;
+
+while (1 < n)
+(
+*(s + 1) = b;
+i++;
+)
+return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is the readme file 0x07. C - Even more pointers, arrays and strings
